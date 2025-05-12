c6604b762d823f28583622cf28648e85ce539d72
Pedro <Eu@exemplo.com>
2025-05-12 11:54:30 -0300

    Faça um programa que leia um número inteiro e mostre na tela o seu dobro, triplo e raiz quadrada.

---
+++ Exercicios em Python/1_Olá Mundo/ex006.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: '))
+
+print(
+    f"Você digitou {n1} \n Seu dobro é: {n1*2} \n Seu triplo é: {n1*3} \n Sua raiz quadrada é: {n1**(1/2):.3f}")
+
+# Exercicio 6
+# Faça um programa que leia um número inteiro e mostre na tela o seu dobro, triplo e raiz quadrada.
