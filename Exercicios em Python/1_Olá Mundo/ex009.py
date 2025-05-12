ae8fd25831ac653ba57f0939e8bf5c6199f92354
Pedro <Eu@exemplo.com>
2025-05-12 18:54:16 -0300

    Tabuada

--- Exercicios em Python/1_Olá Mundo/ex009.py
+++ Exercicios em Python/1_Olá Mundo/ex009.py
@@ -1,8 +1,18 @@
 
 numero = int(input('Digite um número: '))
-for i in range(1, 11):
 
-    print(f'A tabuada de {numero} é:')
-    print(f'{numero} x {i} = {numero * i}')
+print("-" * 12)
+print(f'{numero} x {1:2} = {numero * 1}')
+print(f'{numero} x {2:2} = {numero * 2}')
+print(f'{numero} x {3:2} = {numero * 3}')
+print(f'{numero} x {4:2} = {numero * 4}')
+print(f'{numero} x {5:2} = {numero * 5}')
+print(f'{numero} x {6:2} = {numero * 6}')
+print(f'{numero} x {7:2} = {numero * 7}')
+print(f'{numero} x {8:2} = {numero * 8}')
+print(f'{numero} x {9:2} = {numero * 9}')
+print(f'{numero} x {10:2} = {numero * 10}')
+print("-" * 12)
+
 # Exercicio 9
 # Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
