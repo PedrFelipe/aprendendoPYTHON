b2bd3086e8ff46e097c1e5cba57aa7abc7622f98
Pedro <Eu@exemplo.com>
2025-05-12 12:02:45 -0300

    Faça um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.

---
+++ Exercicios em Python/1_Olá Mundo/ex008.py
@@ -0,0 +1,8 @@
+metros = float(input('Digite um valor em metros: '))
+centimetros = metros * 100
+milimetros = metros * 1000
+
+print(f'O valor de {metros} metros é igual a: \n {centimetros} centímetros. \n {milimetros} milímetros.') 
+
+#Exercicio 8
+# Faça um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
