f78c4aea82958b4bf660ae7f3220f95cdedd6c7c
Pedro <Eu@exemplo.com>
2025-05-12 12:13:51 -0300

    Faça um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.

---
+++ ex010.py
@@ -0,0 +1,10 @@
+n1 = float(input('Quantos reais você tem na carteira? R$'))
+dolar = 5.60
+dolares = n1 / dolar
+
+print(f'Com R${n1:.2f} você pode comprar US${dolares:.2f} dólares.')
+
+
+# Exercicio 10
+# Faça um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+# Considere US$1,00 = R$5,60
