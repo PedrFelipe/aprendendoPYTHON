cabb73b26c3cad30076565a21cbc001884e0b5ec
Pedro <Eu@exemplo.com>
2025-05-12 11:53:34 -0300

    Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.

---
+++ Exercicios em Python/1_Olá Mundo/ex005.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: '))
+
+print(
+    f"Você digitou {n1} \n Seu sucessor é: {n1+1} \n Seu antecessor é: {n1-1}")
+
+# Exercicio 5
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
