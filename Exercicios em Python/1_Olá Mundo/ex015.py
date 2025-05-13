e509039df98b453f684ab87a7169b170eec65c0e
Pedro <Eu@exemplo.com>
2025-05-13 07:41:09 -0300

    Escreva um programa que pergunte a quantidade de dias que um carro foi alugado e a quantidade de km rodados.

---
+++ Exercicios em Python/1_Olá Mundo/ex015.py
@@ -0,0 +1,7 @@
+dias = int(input('Quantos dias alugados? '))
+km = float(input('Quantos km rodados? '))
+preco = (dias * 60) + (km * 0.15)
+print(f'O total a pagar é de R${preco:.2f}')
+
+# Exercicio 15
+# Escreva um programa que pergunte a quantidade de dias que um carro foi alugado e a quantidade de km rodados.
