262c38d13d6e3b6386bf5c846a6197e8c3e03475
Pedro <Eu@exemplo.com>
2025-05-13 07:37:00 -0300

    Escreva um programa que converta uma temperatura digitada em °C e converta para °F.

---
+++ Exercicios em Python/1_Olá Mundo/ex014.py
@@ -0,0 +1,7 @@
+Celsius = float(input("Qual a temperatura em °C?"))
+Farhenheit = (Celsius * 9/5) + 32
+
+print(f"A temperatura de {Celsius}°C corresponde a {Farhenheit}°F")
+
+# Exercicio 14
+# Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
